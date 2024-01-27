f66c0a0cce3fbd11b7649d74c451e81807828ff1
An <an.tran511@outlook.com>
2024-01-27 18:08:53 +0700

    chore(web): migrate from old repo

---
+++ libs/types/package-and-label.ts
@@ -0,0 +1,32 @@
+export type TPackageAndLabel = {
+  id: number
+  name: string
+  uomId: number
+  secondaryUomId: number
+  purchaseUomId: number
+  partnerId: number
+  categoryId: number
+  itemCode: string
+  note: string
+  specs: {
+    dimension: string
+    spreadDimension: string
+    thickness: number
+    numberOfColors: number
+  }
+}
+
+export type TPackageAndLabelMutation = {
+  name: string
+  uomId: number
+  partnerId: number
+  categoryId: number
+  itemCode: string
+  note: string
+  specs: {
+    dimension: string
+    spreadDimension: string
+    thickness: number
+    numberOfColors: number
+  }
+}
